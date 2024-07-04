7812d67e63186e89d98f51569c47d871cd6d62bc
KIRILL <vedgerald1988@tut.by>
2024-07-04 08:44:13 +0300

    created program PrintNaturalNumbers

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿
+
+static void PrintNaturalNumbers(int m, int n)
+    {
+        if (m <= n)
+        {
+            Console.Write(m + " ");
+            PrintNaturalNumbers(m + 1, n);
+        }
+    }
+
+    PrintNaturalNumbers(4, 8);
