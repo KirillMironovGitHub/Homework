0abe05c6289cb747835ebd15c4c7def14e8a0b26
KIRILL <vedgerald1988@tut.by>
2024-07-04 09:07:33 +0300

    Add Function Ackermann

--- Program.cs
+++ Program.cs
@@ -10,3 +10,29 @@ static void PrintNaturalNumbers(int m, int n)
     }
 
     PrintNaturalNumbers(4, 8);
+
+
+
+using System;
+
+class Program
+{
+    static int Ackermann(int m, int n)
+    {
+        if (m == 0)
+            return n + 1;
+        else if (n == 0)
+            return Ackermann(m - 1, 1);
+        else
+            return Ackermann(m - 1, Ackermann(m, n - 1));
+    }
+
+    static void Main()
+    {
+        int m = 3;
+        int n = 2;
+        int result = Ackermann(m, n);
+
+        Console.WriteLine($"Ackermann({m}, {n}) = {result}");
+    }
+}
